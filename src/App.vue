51ce6e38e9e5d9efaf4717a9b1758d0b34a399d5
Marisaliap <mpascal@creditu.com>
2022-12-10 16:56:15 -0300

    updating greetings

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome from Vue-Amplify App"/>
   </div>
 </template>
 
